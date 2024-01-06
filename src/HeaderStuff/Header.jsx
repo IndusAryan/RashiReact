62c907af6c95a8134129f8a5c03c958d21f2bbda
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2024-01-06 17:42:25 +0530

    make header responsive

--- src/HeaderStuff/Header.jsx
+++ src/HeaderStuff/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-      <header className="bg-[#004daa] p-1 flex justify-between items-center rounded-b-lg">
+      <header className="bg-[#004daa] flex justify-between items-center rounded-b-lg">
        
         <div className="flex items-center">
           <img
@@ -10,21 +10,20 @@ function Header() {
             alt="Your Logo"
             className="h-14 w-17"
           />
-          <span className="text-white text-lg font-italic">𝓐𝓼𝓽𝓻𝓸𝓥𝓜</span>
+          <span className="text-white mr-5 ml--2 text-lg font-italic">𝓐𝓼𝓽𝓻𝓸𝓥𝓜</span>
         </div>
   
-        <div className="flex items-center  m-3">
+        <div className="flex items-center m-2">
           <a
             href="https://github.com/IndusAryan/RashiReact"
             target="_blank"
-            rel="noopener noreferrer"
-            className="text-white mr-4 bg-[#003d88] p-1.5 rounded-lg hover:text-gray-300"
+            className="text-white mr-4 bg-[#003d88] p-1 rounded-lg hover:text-gray-300"
           >
             GitHub Repo
           </a>
           <a
             href="https://github.com/IndusAryan" 
-            className="text-white bg-[#003d88] p-1.5 rounded-lg hover:text-gray-300"
+            className="text-white bg-[#003d88] p-1 rounded-lg hover:text-gray-300"
           >
             By IndusAryan 🚩
           </a>
